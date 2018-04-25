02bd716e52505ecdf386a9fdbe1a15e53b92151c
Administrator <Administrator@192.168.0.104>
2018-04-26 00:54:53 +0800

    1

---
+++ src/main/java/com/zyf/mmtipay/MmtipayApplication.java
@@ -0,0 +1,12 @@
+package com.zyf.mmtipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmtipayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmtipayApplication.class, args);
+	}
+}
